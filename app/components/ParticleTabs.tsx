09487f2aa4a176cb5b0710c3147402a57fde32bb
nevermyuk <zequanong@gmail.com>
2024-02-29 16:46:08 +0800

    feat: initial template

---
+++ app/components/ParticleTabs.tsx
@@ -0,0 +1,77 @@
+import { Tab } from "@headlessui/react";
+import { Particles } from "@tsparticles/react";
+import { AnimatePresence, motion } from "framer-motion";
+
+interface ParticleTabsProps {
+  id: string;
+  accentColor: string;
+  children: React.ReactNode;
+}
+
+const ParticleTabs = ({ id, accentColor, children }: ParticleTabsProps) => {
+  return (
+    <Tab as="div" className="w-full focus:outline-none group">
+      {({ selected }) => (
+        <button className="relative w-full py-4 text-white focus:outline-none uppercase text-lg w-full">
+          {children}
+          {selected && (
+            <motion.div
+              className="w-full absolute left-0 right-0 z-10 rounded-full h-[2px] top-0"
+              layoutId="underline"
+              initial={false}
+              animate={{
+                background: accentColor,
+                boxShadow: `0 14px 30px 1px ${accentColor}, 0 4px 12px ${accentColor}, 0 1px 7px ${accentColor}`,
+              }}
+            />
+          )}
+          <AnimatePresence>
+            {selected && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 0.2 }}
+                exit={{ opacity: 0 }}
+                className="absolute inset-0 w-full h-full pointer-events-none"
+              >
+                <Particles
+                  id={id}
+                  options={{
+                    background: {
+                      color: "green",
+                    },
+
+                    particles: {
+                      color: {
+                        value: accentColor,
+                      },
+                      number: {
+                        value: 500,
+                        density: {
+                          enable: true,
+                        },
+                      },
+                      size: {
+                        value: { min: 1, max: 3 },
+                      },
+                      shape: {
+                        type: "circle",
+                      },
+                      move: {
+                        enable: true,
+                        direction: "bottom",
+                        speed: 1,
+                      },
+                    },
+                  }}
+                />
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <div className="absolute inset-0 w-full h-1/3 pointer-events-none bg-gradient-to-b from-gray-800 to-gray-900 transition-opacity opacity-0 group-hover:opacity-30 z-[-1]"></div>
+        </button>
+      )}
+    </Tab>
+  );
+};
+
+export default ParticleTabs;
---
+++ app/projects/page.tsx
@@ -0,0 +1,5 @@
+const ProjectPage = () => {
+  return <h1>Projects</h1>;
+};
+
+export default ProjectPage;
