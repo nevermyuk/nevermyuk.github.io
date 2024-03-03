2ff1b5c6897a11ea2ff1fde83e3a097ae3970b54
nevermyuk <zequanong@gmail.com>
2024-03-03 22:07:32 +0800

    fix: main page background color

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
 "use client";
-import Link from "next/link";
-import ParticlesBackground from "./components/ParticlesBackground";
-import { navItems } from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-
+    <div className="flex flex-col items-center justify-center overflow-hidden ">
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left" />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         quan
       </h1>
-
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">in development</h2>
       </div>
