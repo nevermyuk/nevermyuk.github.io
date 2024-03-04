eebca35641409fcc7c54f3067c8b2f79a8bca77f
nevermyuk <zequanong@gmail.com>
2024-03-04 15:38:05 +0800

    feat: add animation to particle bg

--- components/ParticlesBackground.tsx
+++ components/ParticlesBackground.tsx
@@ -2,6 +2,7 @@
 import { Container } from "@tsparticles/engine";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
+import { motion, useAnimation } from "framer-motion";
 import { useEffect, useState } from "react";
 
 const ParticlesBackground = () => {
@@ -15,87 +16,98 @@ const ParticlesBackground = () => {
     });
   }, []);
 
-  const particlesLoaded = (
-    container?: Container | undefined
-  ): Promise<void> => {
-    return Promise.resolve();
+  const particlesLoaded = async (container?: Container) => {
+    if (container) {
+      controls.start({
+        opacity: 1,
+        transition: {
+          duration: 1,
+        },
+      });
+    }
   };
+
+  const controls = useAnimation();
 
   const colors = ["#6366F1", "#4F46E5", "#0EA5E9", "#0284C7", "#0369A1"];
 
   return (
-    <div className="absolute inset-0">
-      <Particles
-        id="tsparticles"
-        particlesLoaded={particlesLoaded}
-        options={{
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onHover: {
-                enable: true,
-                mode: "repulse",
+    <motion.div animate={controls} className="opacity-0">
+      {init && (
+        <div className="absolute inset-0">
+          <Particles
+            id="tsparticles"
+            particlesLoaded={particlesLoaded}
+            options={{
+              fpsLimit: 120,
+              interactivity: {
+                events: {
+                  onHover: {
+                    enable: true,
+                    mode: "repulse",
+                  },
+                  resize: {
+                    delay: 0.5,
+                    enable: true,
+                  },
+                },
+                modes: {
+                  push: {
+                    quantity: 4,
+                  },
+                  repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                  },
+                },
               },
-              resize: {
-                delay: 0.5,
-                enable: true,
+              particles: {
+                color: {
+                  value: colors,
+                },
+                links: {
+                  color: {
+                    value: colors,
+                  },
+                  distance: 150,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 1,
+                },
+                move: {
+                  direction: "none",
+                  enable: true,
+                  outModes: {
+                    default: "bounce",
+                  },
+                  random: false,
+                  speed: 0.5,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                    width: 1920,
+                    height: 1080,
+                  },
+                  value: 80,
+                },
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: "circle",
+                },
+                size: {
+                  value: { min: 1, max: 5 },
+                },
               },
-            },
-            modes: {
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: colors,
-            },
-            links: {
-              color: {
-                value: colors,
-              },
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 0.5,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                width: 1920,
-                height: 1080,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
-      />
-    </div>
+              detectRetina: true,
+            }}
+          />
+        </div>
+      )}
+    </motion.div>
   );
 };
 
